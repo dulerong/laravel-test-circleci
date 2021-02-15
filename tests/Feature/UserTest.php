907824c7038e447a5ca3ee277a42cebe79502775
Le-Jung Du <lejung@qualias.jp>
2021-02-15 17:08:35 +0900

    added new test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
@@ -32,4 +33,29 @@ class UserTest extends TestCase
                 'message' => "Successfully created user!",
             ]);
     }//testUserCreation
+
+    public function testUserLogin()
+    {
+        $this->artisan('passport:install');
+
+        $name = $this->faker->name();
+        $email = $this->faker->email();
+
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($this->password)
+        ]);        
+        
+        $user->save(); 
+        
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $email,
+            'password' => $this->password
+        ]);
+
+        // $response->assertSee("success");
+        $response->assertStatus(200);
+        $this->assertAuthenticated();
+    }
 }
