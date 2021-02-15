b5cc1bcdb6684ed93e1a954b2725cea1effe516c
Le-Jung Du <lejung@qualias.jp>
2021-02-15 13:40:02 +0900

    2nd commit

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use WithFaker;
+
+    private $password = "password";
+
+    public function testUserCreation()
+    {
+       	
+       	$name = $this->faker->name();
+       	$email = $this->faker->email();
+
+        $response = $this->postJson('/api/auth/signup', [
+            'name' => $name, 
+            'email' => $email,
+            'password' => $this->password, 
+            'password_confirmation' => $this->password
+        ]); 
+
+
+        $response
+            ->assertStatus(201)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
+    }//testUserCreation
+}
